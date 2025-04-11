44169780f203847dbd3c1483ac57558c2148fccd
Himanshu <himanshugaur684@gmail.com>
2025-04-12 00:09:01 +0530

    Nested Navigation

--- app/src/main/java/dev/himanshu/typesafenavigation/MainActivity.kt
+++ app/src/main/java/dev/himanshu/typesafenavigation/MainActivity.kt
@@ -6,16 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import dev.himanshu.typesafenavigation.ui.theme.TypeSafeNavigationTheme
 import kotlinx.serialization.Serializable
@@ -27,6 +26,30 @@ class MainActivity : ComponentActivity() {
         setContent {
             TypeSafeNavigationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = AuthDest.Root) {
+
+                        // auth
+                        navigation<AuthDest.Root>(startDestination = AuthDest.AuthScreen) {
+                            composable<AuthDest.AuthScreen> {
+                                AuthScreen {
+                                    navController.navigate(ProfileDest.ProfileScreen)
+                                }
+
+
+                            }
+                        }
+
+                        //profile
+                        navigation<ProfileDest.Root>(startDestination = ProfileDest.ProfileScreen) {
+                            composable<ProfileDest.ProfileScreen> {
+                                ProfileScreen {
+                                    navController.popBackStack()
+                                }
+                            }
+                        }
+
+                    }
 
                 }
             }
@@ -34,3 +57,46 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+sealed interface AuthDest {
+
+    @Serializable
+    data object Root : AuthDest
+
+    @Serializable
+    data object AuthScreen : AuthDest
+
+
+}
+
+@Composable
+fun AuthScreen(modifier: Modifier = Modifier, onClick: () -> Unit) {
+
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Button(onClick = onClick) {
+            Text("Auth Screen")
+        }
+    }
+
+}
+
+
+sealed interface ProfileDest {
+
+    @Serializable
+    data object Root : ProfileDest
+
+    @Serializable
+    data object ProfileScreen : ProfileDest
+
+}
+
+@Composable
+fun ProfileScreen(modifier: Modifier = Modifier, onClick: () -> Unit) {
+
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Button(onClick = onClick) {
+            Text("PRofile Screen")
+        }
+    }
+
+}
